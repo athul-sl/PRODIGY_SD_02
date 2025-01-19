c71fa8a6789db68c68780006dcca1762fa390ac3
athul-sl <athulsl2505@gmail.com>
2025-01-19 19:13:02 +0530

    random.c

--- random.c
+++ random.c
@@ -7,7 +7,7 @@ int main()
   srand(time(0));
   r=rand()%50+1;
   printf("Guess the number between 1 and 50. ");
-  printf("Enter number : ");
+  printf("\nEnter number : ");
   while(correct==0)
     {
       scanf("%d",&n);
@@ -18,7 +18,7 @@ int main()
         break;}
       else if(n>r)
         printf("\nToo high, try again : ");
-      else
+      else if(n<r)
         printf("\nToo low, try again : ");
     }
 }
